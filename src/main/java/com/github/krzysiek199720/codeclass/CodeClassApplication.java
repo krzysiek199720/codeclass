8f87b41e10cb687f81c46b1cca638bf86946ace4
Krzysztof Staniszewski <krzysiek199720@gmail.com>
2020-07-13 11:22:52 +0200

    initial commit

---
+++ src/main/java/com/github/krzysiek199720/codeclass/CodeClassApplication.java
@@ -0,0 +1,13 @@
+package com.github.krzysiek199720.codeclass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeClassApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeClassApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/krzysiek199720/codeclass/CodeClassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.krzysiek199720.codeclass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeClassApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
