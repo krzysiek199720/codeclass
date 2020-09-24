e43d7b94fa9be3ad63e730373e290ad8615d0e80
Krzysztof Staniszewski <krzysiek199720@gmail.com>
2020-09-24 09:11:18 +0200

    soft debug mode

--- src/main/java/com/github/krzysiek199720/codeclass/core/exceptions/handler/MainExceptionHandler.java
+++ src/main/java/com/github/krzysiek199720/codeclass/core/exceptions/handler/MainExceptionHandler.java
@@ -2,6 +2,7 @@ package com.github.krzysiek199720.codeclass.core.exceptions.handler;
 
 import com.github.krzysiek199720.codeclass.core.exceptions.exception.NotFoundException;
 import com.github.krzysiek199720.codeclass.core.exceptions.response.ErrorResponse;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,8 +13,15 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @Order
 public class MainExceptionHandler {
 
+    @Value("${codeclass.debug:false}")
+    private boolean debugModeEnabled;
+
+
     @ExceptionHandler
-    public ResponseEntity<ErrorResponse> handleException(Exception exc){
+    public ResponseEntity<ErrorResponse> handleException(Exception exc) throws Exception {
+
+        if(debugModeEnabled)
+            throw exc;
 
         ErrorResponse response = new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, "internal_server_error");
 
