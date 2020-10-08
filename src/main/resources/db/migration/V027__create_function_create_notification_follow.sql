b1ccab581e49591176a6ff786c2c48e16a318506
Krzysztof Staniszewski <krzysiek199720@gmail.com>
2020-10-08 12:33:08 +0200

    comment sql fix, create notif sql func

---
+++ src/main/resources/db/migration/V027__create_function_create_notification_follow.sql
@@ -0,0 +1,18 @@
+CREATE OR REPLACE FUNCTION create_follow_notification(
+    course_group_id BIGINT,
+    text_val VARCHAR(30),
+    slug_val VARCHAR(255)
+)
+    RETURNS VOID
+    LANGUAGE plpgsql
+AS $$
+DECLARE user_id BIGINT;
+BEGIN
+    FOR user_id IN
+        SELECT userid AS user_id FROM course.follow WHERE coursegroupid = course_group_id
+        LOOP
+            INSERT INTO notification.notification (text, slug, userid) VALUES (text_val, slug_val, user_id);
+        END LOOP;
+
+    COMMIT;
+END;$$
