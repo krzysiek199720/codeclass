e67c1f45bee7bdd0c4a51f4dacf70e05454a38aa
Krzysztof Staniszewski <krzysiek199720@gmail.com>
2020-11-16 13:12:39 +0100

    Constructors

--- src/main/java/com/github/krzysiek199720/codeclass/course/category/api/CategorySaveApi.java
+++ src/main/java/com/github/krzysiek199720/codeclass/course/category/api/CategorySaveApi.java
@@ -1,8 +1,10 @@
 package com.github.krzysiek199720.codeclass.course.category.api;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@AllArgsConstructor
 @NoArgsConstructor
 @Getter
 public class CategorySaveApi {
--- src/main/java/com/github/krzysiek199720/codeclass/course/language/api/LanguageSaveApi.java
+++ src/main/java/com/github/krzysiek199720/codeclass/course/language/api/LanguageSaveApi.java
@@ -1,8 +1,10 @@
 package com.github.krzysiek199720.codeclass.course.language.api;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@AllArgsConstructor
 @NoArgsConstructor
 @Getter
 public class LanguageSaveApi {
